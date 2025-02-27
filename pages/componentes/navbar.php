a233d1065983830f2073c947677a627b4f15e0ac
kaykymnz <menezeskayky16@gmail.com>
2025-02-26 23:41:59 -0300

    parte da tela inicial feita, navbar e menu lateral iniciado

--- pages/componentes/navbar.php
+++ pages/componentes/navbar.php
@@ -1,1 +1,12 @@
-<div class="header"></div>
+<div class="header">
+    <div class="inv"></div>
+    <div class="logo">
+        <img src="../images/logo.png" alt="logo" width="1039">
+    </div>
+    <div class="menuNavbar" onclick="abrirMenu()">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+
+</div>      
--- pages/index.php
+++ pages/index.php
@@ -3,10 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/main.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <script src="../scripts/menu.js"></script>
+    <div class="ini">
+
+
+    <?php
+        include('componentes/navbar.php');
+        include('componentes/menuLateral.php');
+    ?>
+        
+    <div class="bannerInicial">
+        <img src="../images/BannerInicial.png" alt="">
+    </div>
+    </div>
 </body>
 </html>
