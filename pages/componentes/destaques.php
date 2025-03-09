4836442c6e164d7d8d59083bd9af71b150178677
kaykymnz <menezeskayky16@gmail.com>
2025-03-09 15:49:34 -0300

    criação e estilização da tela de login

--- pages/componentes/destaques.php
+++ pages/componentes/destaques.php
@@ -2,19 +2,24 @@
     <div class="destaqueTxt">Destaques</div>
     <div class="destaquesCarros">
         <div class="destaque">
+        <a href="../login.php">
             <img src="../images/sonic.png" alt="">
             <p>Chevrolet Sonic</p>
             <p>R$ 14.000,00</p>
+        </a>
+
         </div>
         <div class="destaque">
-        <img src="../images/urus.png" alt="">
+        <a href="../login.php">
+            <img src="../images/urus.png" alt="">
             <p>Lamborghini Urus</p>
             <p>R$ 6.344.000,00</p>
+        </a>
     </div>
     </div>
 </div>
 <div class="maisVendido">
-    <div class="vendidoTxt">O mais vendido da Royal Motors</div>
+    <div class="vendidoTxt">Sucesso de vendas da Royal Motors</div>
         <div class="maisVendidoImg">
             <img src="../images/civic.png" alt="">
             <p>Honda Civic</p>
---
+++ pages/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/main.css">
+    <link rel="icon" type="image/png" href="../images/icons/iconPag.png">
+    <title> Royal Motors | Login </title>
+</head>
+<body>
+    <?php 
+        // include('componentes/navbar.php')
+    ?>
+    <div class="formLogin">
+        
+        <form action="acts/login.act.php" method="post" enctype="multipart/form-data">
+                <div class="imgForm"><img src="../images/icons/iconCarroBranco.png" alt=""></div>
+                
+                <div class="inputForm">
+                <p>CPF</p>
+                <input type="number" placeholder="Apenas Números">
+                </div>
+                <div class="inputForm">
+                <p>Senha</p>
+                <input type="text" placeholder="">
+                </div>
+                <p>Caso tenha problemas com o Login, contate seu <strong>Administrador</strong>.</p>
+                <div class="inputForm">
+                <input type="submit" value="Entrar">
+                </div>
+                <p>Faça login como <strong>Administrador</strong> ou <strong>Vendedor.</strong> </p>
+                
+        </form>
+        <div class="imgLadoForm">
+            <img src="../images/imgTelaLogin.jpg" alt="">
+        </div>
+    </div>
+
+</body>
+</html>
