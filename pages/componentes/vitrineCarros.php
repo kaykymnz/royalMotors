613648d3d2098a59f74ca41bc1f81cffbfa89c3e
kaykymnz <menezeskayky16@gmail.com>
2025-05-07 00:20:59 -0300

    alteração no banner inicial, no banner vertical, e estilização da tela de vitrine marcas

--- pages/componentes/vitrineCarros.php
+++ pages/componentes/vitrineCarros.php
@@ -1,13 +1,22 @@
-<?php 
 
+<?php 
+                       
                         extract($_GET);
 
                         require('../db/connect.php');
                         @session_start();
                         $carros = mysqli_query($con, "Select * from `carros` WHERE `marcaCarro` = '$marca'");
-                            echo "<div class=carrosCards>";                        
+                        echo "<div class=vitrineDireita>"; 
+                        echo "<div class=pesquisaCarroFiltro>
+                                <div class=inputPesq>
+                                        <img src=../images/icons/lupa.png alt=erro>
+                                        <input type=text placeholder=\"Procure aqui o carro dos seus sonhos\">
+                                </div>
+                                <img src=../images/icons/ordenar.png alt=erro>
+                             </div>";
+                        echo "<div class=carrosCards>";  
                             while($carro=mysqli_fetch_array($carros)){
-                                
+                        
                             echo "<div class=carroCard>
                                             <a href=pagVitrineCarro.php?cod=$carro[idCarro]>
                                             <p ><img src=../images/carrosVenda/$carro[imgPrincipalCarro]></p>
@@ -20,5 +29,6 @@
                                             </a>
                                  </div>";
                         }
+                         echo "</div>";
                                 echo "</div>"
                         ?>
